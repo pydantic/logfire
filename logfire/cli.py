3aa3efa57706ab90972bc026df8dab447cc6a5db
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-10-03 10:30:12 +0200

    [PYD-284] Create logfire CLI (#262)

---
+++ logfire/cli.py
@@ -0,0 +1,56 @@
+import platform
+import shutil
+from pathlib import Path
+
+from rich.console import Console
+from typer import Exit, Option, Typer, confirm, echo
+
+from logfire.config import LogfireCredentials
+from logfire.version import VERSION
+
+app = Typer(help='The CLI for Logfire. 🚀')
+
+# NOTE: If the app is divided into multiple files, use the typer.Context to share the Console object.
+console = Console()
+
+
+def version_callback(value: bool) -> None:
+    if value:
+        py_impl = platform.python_implementation()
+        py_version = platform.python_version()
+        system = platform.system()
+        echo(f'Running Logfire {VERSION} with {py_impl} {py_version} on {system}.')
+        raise Exit(0)
+
+
+@app.callback()
+def main(
+    version: bool = Option(
+        None,
+        '--version',
+        callback=version_callback,
+        is_eager=True,
+        help='Show version and exit.',
+    ),
+):
+    ...
+
+
+@app.command(help='Get your dashboard url and project name.')
+def whoami(logfire_dir: Path = Path('.logfire')):
+    credentials = LogfireCredentials.load_creds_file(logfire_dir)
+
+    if credentials is None:
+        console.print('Data not found.')
+    else:
+        console.print(f'Your dashboard url is: {credentials.dashboard_url}')
+        console.print(f'Your project name is: [bold]{credentials.project_name}.')
+
+
+@app.command(help='Clean logfire data.')
+def clean(logfire_dir: Path = Path('.logfire')):
+    if confirm(f'The folder {logfire_dir} will be deleted. Are you sure?'):
+        shutil.rmtree(logfire_dir)
+        echo('Cleaned logfire data.')
+    else:
+        echo('Clean aborted.')
---
+++ tests/test_cli.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import pytest
+from typer.testing import CliRunner
+
+from logfire.cli import app
+from logfire.config import LogfireCredentials
+from logfire.version import VERSION
+
+
+@pytest.fixture
+def logfire_credentials() -> LogfireCredentials:
+    return LogfireCredentials(token='token', project_name='my-project', dashboard_url='https://dashboard.logfire.dev')
+
+
+def test_version() -> None:
+    runner = CliRunner()
+    result = runner.invoke(app, ['--version'])
+    assert result.exit_code == 0
+    assert VERSION in result.stdout
+
+
+def test_whoami(logfire_credentials: LogfireCredentials) -> None:
+    runner = CliRunner()
+    with runner.isolated_filesystem() as tmp_dir:
+        logfire_credentials.write_creds_file(Path(tmp_dir))
+        result = runner.invoke(app, ['whoami', '--logfire-dir', tmp_dir])
+        assert result.exit_code == 0
+        assert logfire_credentials.dashboard_url in result.stdout
+        assert logfire_credentials.project_name in result.stdout
+
+
+def test_whoami_without_data() -> None:
+    runner = CliRunner()
+    with runner.isolated_filesystem() as tmp_dir:
+        result = runner.invoke(app, ['whoami', '--logfire-dir', tmp_dir])
+        assert result.exit_code == 0
+        assert 'Data not found.' in result.stdout
+
+
+def test_whoami_default_dir(logfire_credentials: LogfireCredentials) -> None:
+    runner = CliRunner()
+    with runner.isolated_filesystem() as tmp_dir:
+        logfire_credentials.write_creds_file(Path(tmp_dir) / '.logfire')
+        result = runner.invoke(app, ['whoami'])
+        assert result.exit_code == 0
+        assert logfire_credentials.dashboard_url in result.stdout
+        assert logfire_credentials.project_name in result.stdout
+
+
+def test_clean(logfire_credentials: LogfireCredentials) -> None:
+    runner = CliRunner()
+    with runner.isolated_filesystem() as tmp_dir:
+        logfire_credentials.write_creds_file(Path(tmp_dir))
+        assert Path(tmp_dir).exists()
+        result = runner.invoke(app, ['clean', '--logfire-dir', tmp_dir], input='y')
+        assert result.exit_code == 0
+        assert 'Cleaned logfire data.' in result.stdout
+        assert not Path(tmp_dir).exists()
