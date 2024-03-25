562be397bbc2d0ab669cc6fd2fbee75317249a72
Alex Hall <alex.mojaki@gmail.com>
2024-03-25 09:36:48 +0000

    Don't configure logging in Django test site (#1796)

--- tests/otel_integrations/django_test_project/django_test_site/settings.py
+++ tests/otel_integrations/django_test_project/django_test_site/settings.py
@@ -122,24 +122,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGGING = {
-    'version': 1,
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        }
-    },
-}
+LOGGING_CONFIG = None
