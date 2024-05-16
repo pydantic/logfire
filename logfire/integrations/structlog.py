c0fb7ddde3bbdf6d7bfa63e50a9f68c300cd2a0a
Alex Hall <alex.mojaki@gmail.com>
2024-05-16 13:20:13 +0200

    Handle logging non-string in structlog

--- logfire/integrations/structlog.py
+++ logfire/integrations/structlog.py
@@ -4,6 +4,7 @@ from structlog.types import EventDict, WrappedLogger
 
 import logfire
 
+from .._internal.constants import ATTRIBUTES_MESSAGE_KEY
 from .logging import RESERVED_ATTRS as LOGGING_RESERVED_ATTRS
 
 RESERVED_ATTRS = LOGGING_RESERVED_ATTRS | {'level', 'event', 'timestamp'}
@@ -21,10 +22,10 @@ class LogfireProcessor:
         attributes = {k: v for k, v in event_dict.items() if k not in RESERVED_ATTRS}
         level = event_dict.get('level', 'info').lower()
         # NOTE: An event can be `None` in structlog. We may want to create a default msg in those cases.
-        msg_template = event_dict.get('event') or 'structlog event'
+        attributes[ATTRIBUTES_MESSAGE_KEY] = message = event_dict.get('event') or 'structlog event'
         logfire.log(
             level=level,  # type: ignore
-            msg_template=msg_template,
+            msg_template=message,
             attributes=attributes,
             console_log=self.console_log,
             custom_scope_suffix='structlog',
--- tests/test_structlog.py
+++ tests/test_structlog.py
@@ -30,11 +30,13 @@ def logger() -> Any:
 
 
 def test_structlog(exporter: TestExporter, logger: Logger) -> None:
-    logger.info('This is now being logged.')
+    logger.info('This is now being logged: %s', 123)
+    logger.error(456)
+
     assert exporter.exported_spans_as_dict(fixed_line_number=None) == snapshot(
         [
             {
-                'name': 'This is now being logged.',
+                'name': 'This is now being logged: 123',
                 'context': {'trace_id': 1, 'span_id': 1, 'is_remote': False},
                 'parent': None,
                 'start_time': 1000000000,
@@ -42,13 +44,30 @@ def test_structlog(exporter: TestExporter, logger: Logger) -> None:
                 'attributes': {
                     'logfire.span_type': 'log',
                     'logfire.level_num': 9,
-                    'logfire.msg_template': 'This is now being logged.',
-                    'logfire.msg': 'This is now being logged.',
+                    'logfire.msg_template': 'This is now being logged: 123',
+                    'logfire.msg': 'This is now being logged: 123',
                     'code.filepath': 'test_structlog.py',
                     'code.function': 'test_structlog',
                     'code.lineno': 33,
                     'logfire.disable_console_log': True,
                 },
-            }
+            },
+            {
+                'name': '456',
+                'context': {'trace_id': 2, 'span_id': 2, 'is_remote': False},
+                'parent': None,
+                'start_time': 2000000000,
+                'end_time': 2000000000,
+                'attributes': {
+                    'logfire.span_type': 'log',
+                    'logfire.level_num': 17,
+                    'logfire.msg_template': '456',
+                    'logfire.msg': '456',
+                    'code.filepath': 'test_structlog.py',
+                    'code.function': 'test_structlog',
+                    'code.lineno': 34,
+                    'logfire.disable_console_log': True,
+                },
+            },
         ]
     )
