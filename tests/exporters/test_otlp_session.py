fddc4a4cf8e5a3ebd02a4309261f0388693a48e3
Alex Hall <alex.mojaki@gmail.com>
2024-06-07 20:07:50 +0200

    Cleanup and comments

--- tests/exporters/test_otlp_session.py
+++ tests/exporters/test_otlp_session.py
@@ -58,7 +58,11 @@ def test_connection_error_retries(monkeypatch: pytest.MonkeyPatch, caplog: pytes
     session = OTLPExporterHttpSession(max_body_size=10)
     headers = {'User-Agent': 'logfire', 'Authorization': 'Bearer 123'}
     session.headers.update(headers)
+
+    # The main session always fails so that it defers to the retryer.
     session.mount('http://', ConnectionErrorAdapter(Mock(side_effect=requests.exceptions.ConnectionError())))
+
+    # The retryer sessions fails at first to simulate logfire being down, then succeeds.
     success = Response()
     success.status_code = 200
     session.retryer.session.mount(
@@ -66,16 +70,24 @@ def test_connection_error_retries(monkeypatch: pytest.MonkeyPatch, caplog: pytes
         ConnectionErrorAdapter(Mock(side_effect=[requests.exceptions.ConnectionError()] * 10 + [success] * 10)),
     )
 
+    # Create a bunch of failed exports.
     for _ in range(10):
         with pytest.raises(requests.exceptions.ConnectionError):
             session.post('http://example.com/', data=b'123')
 
+    # Wait for the retryer to finish.
+    # time.sleep has been mocked to return 0 so this shouldn't take long.
     assert session.retryer.thread
     session.retryer.thread.join()
+
+    # Check that everything is cleaned up after succeeding.
     assert not session.retryer.tasks
     assert not session.retryer.thread
     assert not list(session.retryer.dir.iterdir())
 
+    # random.random is mocked to return 0.5 so that the retry delay is always 1.5 * 2 ** n.
+    # This means these numbers show the average time slept for each call,
+    # e.g. 6.0 means the actual sleep would be between 4 and 8 seconds.
     assert [call.args for call in sleep_mock.call_args_list] == [
         (1.5,),
         (3.0,),
@@ -84,11 +96,21 @@ def test_connection_error_retries(monkeypatch: pytest.MonkeyPatch, caplog: pytes
         (24.0,),
         (48.0,),
         (96.0,),
+        # This is where we reach the MAX_DELAY of 128 seconds.
         (192.0,),
         (192.0,),
         (192.0,),
         (192.0,),
+        # The errors stop here and requests succeed, so the sleep time is reset.
+        # There are 10 exports and the first one succeeded after the last 192s wait,
+        # so that leaves 9 more short sleeps.
     ] + [(1.5,)] * 9
 
+    # A message gets logged once per minute when an export fails.
+    # time.monotonic is mocked to return a value increasing by 30 each time,
+    # so for 10 failed exports we get 5 messages.
     assert len(caplog.messages) == 5
+    # This will always be the first message in case of failures.
+    # After that the number of failed exports is unpredictable because the main thread is adding to it
+    # at the same time as the retryer thread removes from it.
     assert caplog.messages[0] == 'Currently retrying 1 failed export(s)'
