9bf994eddef4158c2a329f762046482c2e33fea2
Alex Hall <alex.mojaki@gmail.com>
2024-06-18 20:24:16 +0200

    comments

--- logfire/_internal/exporters/tail_sampling.py
+++ logfire/_internal/exporters/tail_sampling.py
@@ -27,7 +27,7 @@ class TailSamplingOptions:
 
     duration: float | None = 1.0
     """
-    Include all spans/logs with duration greater than this level.
+    Include all spans/logs with duration greater than this duration in seconds.
     If None, spans are not included based on duration.
     """
 
@@ -45,6 +45,8 @@ class TraceBuffer:
 
 
 class TailSamplingProcessor(WrapperSpanProcessor):
+    """Passes spans to the wrapped processor if any span in a trace meets the sampling criteria."""
+
     def __init__(self, processor: SpanProcessor, options: TailSamplingOptions, random_rate: float) -> None:
         super().__init__(processor)
         self.duration: float = (
@@ -52,7 +54,14 @@ class TailSamplingProcessor(WrapperSpanProcessor):
         )
         self.level: float | int = float('inf') if options.level is None else LEVEL_NUMBERS[options.level]
         self.random_rate = random_rate
+
+        # A TraceBuffer is typically created for each new trace.
+        # If a span meets the sampling criteria, the buffer is dropped and all spans within are pushed
+        # to the wrapped processor.
+        # So when more spans arrive and there's no buffer, they get passed through immediately.
         self.traces: dict[int, TraceBuffer] = {}
+
+        # Code that touches self.traces and its contents should be protected by this lock.
         self.lock = Lock()
 
     def on_start(self, span: Span, parent_context: context.Context | None = None) -> None:
@@ -60,22 +69,33 @@ class TailSamplingProcessor(WrapperSpanProcessor):
         buffer = None
 
         with self.lock:
+            # span.context could supposedly be None, not sure how.
             if span.context:  # pragma: no branch
                 trace_id = span.context.trace_id
+                # If span.parent is None, it's the root span of a trace.
+                # If random.random() <= self.random_rate, immediately include this trace,
+                # meaning no buffer for it.
                 if span.parent is None and random.random() > self.random_rate:
                     self.traces[trace_id] = TraceBuffer([], [])
 
                 buffer = self.traces.get(trace_id)
                 if buffer is not None:
+                    # This trace's spans haven't met the criteria yet, so add this span to the buffer.
                     buffer.started.append((span, parent_context))
                     dropped = self.check_span(span, buffer)
+                # The opposite case is handled outside the lock since it may take some time.
 
+        # This code may take longer since it calls the wrapped processor which might do anything.
+        # It shouldn't be inside the lock to avoid blocking other threads.
+        # Since it's not in the lock, it shouldn't touch self.traces or its contents.
         if buffer is None:
             super().on_start(span, parent_context)
         elif dropped:
             self.push_buffer(buffer)
 
     def on_end(self, span: ReadableSpan) -> None:
+        # This has a very similar structure and reasoning to on_start.
+
         dropped = False
         buffer = None
 
@@ -87,6 +107,8 @@ class TailSamplingProcessor(WrapperSpanProcessor):
                     buffer.ended.append(span)
                     dropped = self.check_span(span, buffer)
                     if span.parent is None:
+                        # This is the root span, so the trace is hopefully complete.
+                        # Delete the buffer to save memory.
                         self.traces.pop(trace_id, None)
 
         if buffer is None:
@@ -95,6 +117,8 @@ class TailSamplingProcessor(WrapperSpanProcessor):
             self.push_buffer(buffer)
 
     def check_span(self, span: ReadableSpan, buffer: TraceBuffer) -> bool:
+        """If the span meets the sampling criteria, drop the buffer and return True. Otherwise, return False."""
+        # span.end_time and span.start_time are in nanoseconds and can be None.
         if (span.end_time or span.start_time or 0) - (buffer.first_span.start_time or float('inf')) > self.duration:
             self.drop_buffer(buffer)
             return True
