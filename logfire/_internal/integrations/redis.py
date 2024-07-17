caed08eaaf7f6b9c6d16f60412e9b74f76767a2a
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-07-17 15:45:37 +0200

    Add type hints to instrument methods (#320)

--- logfire/_internal/integrations/redis.py
+++ logfire/_internal/integrations/redis.py
@@ -1,9 +1,30 @@
-from typing import Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
 
 from opentelemetry.instrumentation.redis import RedisInstrumentor
 
+if TYPE_CHECKING:
+    from opentelemetry.metrics import MeterProvider
+    from opentelemetry.trace import Span, TracerProvider
+    from redis import Connection
+    from typing_extensions import Protocol, TypedDict, Unpack
 
-def instrument_redis(**kwargs: Any):
+    class RequestHook(Protocol):
+        def __call__(self, span: Span, instance: Connection, *args: Any, **kwargs: Any) -> None: ...
+
+    class ResponseHook(Protocol):
+        def __call__(self, span: Span, instance: Connection, response: Any) -> None: ...
+
+    class RedisInstrumentKwargs(TypedDict, total=False):
+        request_hook: RequestHook | None
+        response_hook: ResponseHook | None
+        tracer_provider: TracerProvider | None
+        meter_provider: MeterProvider | None
+        skip_dep_check: bool
+
+
+def instrument_redis(**kwargs: Unpack[RedisInstrumentKwargs]) -> None:
     """Instrument the `redis` module so that spans are automatically created for each operation.
 
     See the `Logfire.instrument_redis` method for details.
