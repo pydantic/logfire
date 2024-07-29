ec32edca5881f56957b722af87ff935ec09d4192
Alex Hall <alex.mojaki@gmail.com>
2024-07-29 22:11:28 +0200

    internal_error_handling folder

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,9 +3,9 @@ import requests
 import requests_mock
 from inline_snapshot import snapshot
 
-from logfire._internal import internal_error_tb_sample
+import logfire._internal.stack_info
 from logfire._internal.utils import UnexpectedResponse, handle_internal_errors
-from tests.import_used_for_tests import handle_internal_error_example
+from tests.import_used_for_tests.internal_error_handling import internal_logfire_code_example, user_code_example
 
 
 def test_raise_for_status() -> None:
@@ -27,75 +27,77 @@ def test_reraise_internal_exception():
 
 
 def test_internal_exception_tb(caplog: pytest.LogCaptureFixture):
-    handle_internal_error_example.user1()
-    assert [
+    logfire._internal.stack_info.NON_USER_CODE_PREFIXES += (internal_logfire_code_example.__file__,)
+    user_code_example.user1()
+    tracebacks = [
         r.exc_text.replace(  # type: ignore
-            handle_internal_error_example.__file__,
-            'handle_internal_error_example.py',
+            user_code_example.__file__,
+            'user_code_example.py',
         ).replace(
-            internal_error_tb_sample.__file__,
-            'internal_error_tb_sample.py',
+            internal_logfire_code_example.__file__,
+            'internal_logfire_code_example.py',
         )
         for r in caplog.records
-    ] == snapshot(
+    ]
+    assert tracebacks == snapshot(
         [
             """\
 Traceback (most recent call last):
-  File "handle_internal_error_example.py", line 22, in user4
+  File "user_code_example.py", line 22, in user4
     user5()
-  File "handle_internal_error_example.py", line 26, in user5
+  File "user_code_example.py", line 26, in user5
     user6()
-  File "handle_internal_error_example.py", line 30, in user6
-    _outer2(_using_decorator)
-  File "internal_error_tb_sample.py", line 36, in _outer2
-    _outer1(func)
-  File "internal_error_tb_sample.py", line 32, in _outer1
+  File "user_code_example.py", line 30, in user6
+    outer2(using_decorator)
+  File "internal_logfire_code_example.py", line 36, in outer2
+    outer1(func)
+  File "internal_logfire_code_example.py", line 32, in outer1
     func()
-  File "internal_error_tb_sample.py", line 16, in _using_decorator
-    _inner2()
-  File "internal_error_tb_sample.py", line 11, in _inner2
-    _inner1()
-  File "internal_error_tb_sample.py", line 7, in _inner1
+  File "internal_logfire_code_example.py", line 16, in using_decorator
+    inner2()
+  File "internal_logfire_code_example.py", line 11, in inner2
+    inner1()
+  File "internal_logfire_code_example.py", line 7, in inner1
     raise ValueError('inner1')
 ValueError: inner1\
 """,
             """\
 Traceback (most recent call last):
-  File "handle_internal_error_example.py", line 22, in user4
+  File "user_code_example.py", line 22, in user4
     user5()
-  File "handle_internal_error_example.py", line 26, in user5
+  File "user_code_example.py", line 26, in user5
     user6()
-  File "handle_internal_error_example.py", line 31, in user6
-    _outer2(_using_context_manager)
-  File "internal_error_tb_sample.py", line 36, in _outer2
-    _outer1(func)
-  File "internal_error_tb_sample.py", line 32, in _outer1
+  File "user_code_example.py", line 31, in user6
+    outer2(using_context_manager)
+  File "internal_logfire_code_example.py", line 36, in outer2
+    outer1(func)
+  File "internal_logfire_code_example.py", line 32, in outer1
     func()
-  File "internal_error_tb_sample.py", line 21, in _using_context_manager
-    _inner2()
-  File "internal_error_tb_sample.py", line 11, in _inner2
-    _inner1()
-  File "internal_error_tb_sample.py", line 7, in _inner1
+  File "internal_logfire_code_example.py", line 21, in using_context_manager
+    inner2()
+  File "internal_logfire_code_example.py", line 11, in inner2
+    inner1()
+  File "internal_logfire_code_example.py", line 7, in inner1
     raise ValueError('inner1')
 ValueError: inner1\
 """,
             """\
 Traceback (most recent call last):
-  File "handle_internal_error_example.py", line 22, in user4
+  File "user_code_example.py", line 22, in user4
     user5()
-  File "handle_internal_error_example.py", line 26, in user5
+  File "user_code_example.py", line 26, in user5
     user6()
-  File "handle_internal_error_example.py", line 32, in user6
-    _outer2(_using_try_except)
-  File "internal_error_tb_sample.py", line 36, in _outer2
-    _outer1(func)
-  File "internal_error_tb_sample.py", line 32, in _outer1
+  File "user_code_example.py", line 32, in user6
+    outer2(using_try_except)
+  File "internal_logfire_code_example.py", line 36, in outer2
+    outer1(func)
+  File "internal_logfire_code_example.py", line 32, in outer1
     func()
-  File "internal_error_tb_sample.py", line 26, in _using_try_except
-    _inner2()
-  File "internal_error_tb_sample.py", line 11, in _inner2
-    _inner1()
-  File "internal_error_tb_sample.py", line 7, in _inner1
+  File "internal_logfire_code_example.py", line 26, in using_try_except
+    inner2()
+  File "internal_logfire_code_example.py", line 11, in inner2
+    inner1()
+  File "internal_logfire_code_example.py", line 7, in inner1
     raise ValueError('inner1')
 ValueError: inner1\
 """,
