fcc2106d3af55bc469e7a62ba847178b8009032a
Alex Hall <alex.mojaki@gmail.com>
2024-08-06 18:10:18 +0200

    Skip redis tests in 3.8

--- tests/otel_integrations/test_redis.py
+++ tests/otel_integrations/test_redis.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Any, Iterator
 
 import pytest
@@ -9,6 +10,9 @@ from testcontainers.redis import RedisContainer
 
 import logfire
 from logfire.testing import TestExporter
+
+# TODO find a better solution
+pytestmark = pytest.mark.skipif(sys.version_info < (3, 9), reason='Redis testcontainers has problems in 3.8')
 
 
 @pytest.fixture(scope='module', autouse=True)
