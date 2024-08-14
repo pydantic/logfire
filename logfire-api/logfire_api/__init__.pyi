8cf259d503f1b2b9ed7f4903b264553f09cdcd7c
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2024-08-13 22:42:49 -0700

    rye run generate-stubs

--- logfire-api/logfire_api/__init__.pyi
+++ logfire-api/logfire_api/__init__.pyi
@@ -10,41 +10,41 @@ from ._internal.utils import suppress_instrumentation as suppress_instrumentatio
 from .integrations.logging import LogfireLoggingHandler as LogfireLoggingHandler
 from .integrations.structlog import LogfireProcessor as StructlogProcessor
 from .version import VERSION as VERSION
+from _typeshed import Incomplete
 
 __all__ = ['Logfire', 'LogfireSpan', 'LevelName', 'ConsoleOptions', 'PydanticPlugin', 'configure', 'span', 'instrument', 'log', 'trace', 'debug', 'notice', 'info', 'warn', 'error', 'exception', 'fatal', 'force_flush', 'log_slow_async_callbacks', 'install_auto_tracing', 'instrument_fastapi', 'instrument_openai', 'instrument_anthropic', 'instrument_asyncpg', 'instrument_httpx', 'instrument_celery', 'instrument_requests', 'instrument_psycopg', 'instrument_django', 'instrument_flask', 'instrument_starlette', 'instrument_aiohttp_client', 'instrument_sqlalchemy', 'instrument_redis', 'instrument_pymongo', 'instrument_mysql', 'AutoTraceModule', 'with_tags', 'with_settings', 'shutdown', 'load_spans_from_file', 'no_auto_trace', 'METRICS_PREFERRED_TEMPORALITY', 'ScrubMatch', 'ScrubbingOptions', 'VERSION', 'suppress_instrumentation', 'StructlogProcessor', 'LogfireLoggingHandler', 'TailSamplingOptions']
 
-DEFAULT_LOGFIRE_INSTANCE = Logfire()
-span = DEFAULT_LOGFIRE_INSTANCE.span
-instrument = DEFAULT_LOGFIRE_INSTANCE.instrument
-force_flush = DEFAULT_LOGFIRE_INSTANCE.force_flush
-log_slow_async_callbacks = DEFAULT_LOGFIRE_INSTANCE.log_slow_async_callbacks
-install_auto_tracing = DEFAULT_LOGFIRE_INSTANCE.install_auto_tracing
-instrument_fastapi = DEFAULT_LOGFIRE_INSTANCE.instrument_fastapi
-instrument_openai = DEFAULT_LOGFIRE_INSTANCE.instrument_openai
-instrument_anthropic = DEFAULT_LOGFIRE_INSTANCE.instrument_anthropic
-instrument_asyncpg = DEFAULT_LOGFIRE_INSTANCE.instrument_asyncpg
-instrument_httpx = DEFAULT_LOGFIRE_INSTANCE.instrument_httpx
-instrument_celery = DEFAULT_LOGFIRE_INSTANCE.instrument_celery
-instrument_requests = DEFAULT_LOGFIRE_INSTANCE.instrument_requests
-instrument_psycopg = DEFAULT_LOGFIRE_INSTANCE.instrument_psycopg
-instrument_django = DEFAULT_LOGFIRE_INSTANCE.instrument_django
-instrument_flask = DEFAULT_LOGFIRE_INSTANCE.instrument_flask
-instrument_starlette = DEFAULT_LOGFIRE_INSTANCE.instrument_starlette
-instrument_aiohttp_client = DEFAULT_LOGFIRE_INSTANCE.instrument_aiohttp_client
-instrument_sqlalchemy = DEFAULT_LOGFIRE_INSTANCE.instrument_sqlalchemy
-instrument_redis = DEFAULT_LOGFIRE_INSTANCE.instrument_redis
-instrument_pymongo = DEFAULT_LOGFIRE_INSTANCE.instrument_pymongo
-instrument_mysql = DEFAULT_LOGFIRE_INSTANCE.instrument_mysql
-shutdown = DEFAULT_LOGFIRE_INSTANCE.shutdown
-with_tags = DEFAULT_LOGFIRE_INSTANCE.with_tags
-with_settings = DEFAULT_LOGFIRE_INSTANCE.with_settings
-log = DEFAULT_LOGFIRE_INSTANCE.log
-trace = DEFAULT_LOGFIRE_INSTANCE.trace
-debug = DEFAULT_LOGFIRE_INSTANCE.debug
-info = DEFAULT_LOGFIRE_INSTANCE.info
-notice = DEFAULT_LOGFIRE_INSTANCE.notice
-warn = DEFAULT_LOGFIRE_INSTANCE.warn
-error = DEFAULT_LOGFIRE_INSTANCE.error
-fatal = DEFAULT_LOGFIRE_INSTANCE.fatal
-exception = DEFAULT_LOGFIRE_INSTANCE.exception
+span: Incomplete
+instrument: Incomplete
+force_flush: Incomplete
+log_slow_async_callbacks: Incomplete
+install_auto_tracing: Incomplete
+instrument_fastapi: Incomplete
+instrument_openai: Incomplete
+instrument_anthropic: Incomplete
+instrument_asyncpg: Incomplete
+instrument_httpx: Incomplete
+instrument_celery: Incomplete
+instrument_requests: Incomplete
+instrument_psycopg: Incomplete
+instrument_django: Incomplete
+instrument_flask: Incomplete
+instrument_starlette: Incomplete
+instrument_aiohttp_client: Incomplete
+instrument_sqlalchemy: Incomplete
+instrument_redis: Incomplete
+instrument_pymongo: Incomplete
+instrument_mysql: Incomplete
+shutdown: Incomplete
+with_tags: Incomplete
+with_settings: Incomplete
+log: Incomplete
+trace: Incomplete
+debug: Incomplete
+info: Incomplete
+notice: Incomplete
+warn: Incomplete
+error: Incomplete
+fatal: Incomplete
+exception: Incomplete
 __version__ = VERSION
