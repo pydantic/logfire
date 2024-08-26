74140ae772ce69174e60c5d10cc6da8b452a3340
Alex Hall <alex.mojaki@gmail.com>
2024-08-26 14:52:06 +0200

    Update generated stubs

--- logfire-api/logfire_api/_internal/config.pyi
+++ logfire-api/logfire_api/_internal/config.pyi
@@ -124,6 +124,7 @@ class _LogfireConfigData:
     token: str | None
     project_name: str | None
     service_name: str
+    service_version: str | None
     trace_sample_rate: float
     console: ConsoleOptions | Literal[False] | None
     show_summary: bool
