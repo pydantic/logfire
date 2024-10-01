4eada90f1b32697f8652f1e45d6b7251f8de9b52
Alex Hall <alex.mojaki@gmail.com>
2024-10-01 18:25:47 +0200

    fix sqlalchemy instrumentation

--- examples/python/flask-sqlalchemy/main.py
+++ examples/python/flask-sqlalchemy/main.py
@@ -5,7 +5,6 @@ logfire.install_auto_tracing(modules=['app'], min_duration=0)
 from app import app, db  # noqa  # needs to be imported after install_auto_tracing
 
 logfire.configure()
-logfire.instrument_sqlalchemy()
 logfire.instrument_flask(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'  # in-memory database
@@ -14,6 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 with app.app_context():
+    logfire.instrument_sqlalchemy(engine=db.engine)
     db.create_all()
 
 app.run(debug=True)
