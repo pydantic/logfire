108d160bff658bcfffd7b04e509ad6344fe37b1f
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-11-13 17:27:05 +0100

    Add tests

--- tests/otel_integrations/test_pymongo.py
+++ tests/otel_integrations/test_pymongo.py
@@ -1,6 +1,14 @@
+from __future__ import annotations
+
+import importlib
+from unittest import mock
+
+import pytest
+from inline_snapshot import snapshot
 from pymongo import monitoring
 
 import logfire
+import logfire._internal.integrations.pymongo
 
 
 # TODO real test
@@ -9,3 +17,14 @@ def test_instrument_pymongo():
     assert len(command_listeners) == 0  # type: ignore
     logfire.instrument_pymongo()
     assert len(command_listeners) == 1  # type: ignore
+
+
+def test_missing_opentelemetry_dependency() -> None:
+    with mock.patch.dict('sys.modules', {'opentelemetry.instrumentation.pymongo': None}):
+        with pytest.raises(RuntimeError) as exc_info:
+            importlib.reload(logfire._internal.integrations.pymongo)
+        assert str(exc_info.value) == snapshot("""\
+`logfire.instrument_pymongo()` requires the `opentelemetry-instrumentation-pymongo` package.
+You can install this with:
+    pip install 'logfire[pymongo]'\
+""")
