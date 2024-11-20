fb08ac00886ab4ad1a1cc0782c3173f314e982b1
Alex Hall <alex.mojaki@gmail.com>
2024-11-20 13:18:10 +0200

    update stubs for auto-tracing

--- logfire-api/logfire_api/_internal/stack_info.pyi
+++ logfire-api/logfire_api/_internal/stack_info.pyi
@@ -32,6 +32,7 @@ def is_user_code(code: CodeType) -> bool:
         - the standard library
         - site-packages (specifically wherever opentelemetry is installed)
         - the logfire package
+        - an unknown location (e.g. a dynamically generated code object) indicated by a filename starting with '<'
     - It is a list/dict/set comprehension.
         These are artificial frames only created before Python 3.12,
         and they are always called directly from the enclosing function so it makes sense to skip them.
