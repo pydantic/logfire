34bad64aa54a0fa3d271e3a14e4e4538068fa973
Alex Hall <alex.mojaki@gmail.com>
2024-11-20 13:12:41 +0200

    Bump version to v2.4.0

--- logfire-api/logfire_api/_internal/main.pyi
+++ logfire-api/logfire_api/_internal/main.pyi
@@ -39,7 +39,7 @@ from opentelemetry.util import types as otel_types
 from starlette.applications import Starlette
 from starlette.requests import Request as Request
 from starlette.websockets import WebSocket as WebSocket
-from typing import Any, Callable, ContextManager, Iterable, Literal, Sequence, TypeVar
+from typing import Any, Callable, ContextManager, Iterable, Literal, Sequence, TypeVar, overload
 from typing_extensions import LiteralString, ParamSpec, Unpack
 from wsgiref.types import WSGIApplication
 
@@ -223,28 +223,10 @@ class Logfire:
             attributes: The arguments to include in the span and format the message template with.
                 Attributes starting with an underscore are not allowed.
         """
-    def instrument(self, msg_template: LiteralString | None = None, *, span_name: str | None = None, extract_args: bool | Iterable[str] = True, allow_generator: bool = False) -> Callable[[Callable[P, R]], Callable[P, R]]:
-        """Decorator for instrumenting a function as a span.
-
-        ```py
-        import logfire
-
-        logfire.configure()
-
-
-        @logfire.instrument('This is a span {a=}')
-        def my_function(a: int):
-            logfire.info('new log {a=}', a=a)
-        ```
-
-        Args:
-            msg_template: The template for the span message. If not provided, the module and function name will be used.
-            span_name: The span name. If not provided, the `msg_template` will be used.
-            extract_args: By default, all function call arguments are logged as span attributes.
-                Set to `False` to disable this, or pass an iterable of argument names to include.
-            allow_generator: Set to `True` to prevent a warning when instrumenting a generator function.
-                Read https://logfire.pydantic.dev/docs/guides/advanced/generators/#using-logfireinstrument first.
-        """
+    @overload
+    def instrument(self, msg_template: LiteralString | None = None, *, span_name: str | None = None, extract_args: bool | Iterable[str] = True, allow_generator: bool = False) -> Callable[[Callable[P, R]], Callable[P, R]]: ...
+    @overload
+    def instrument(self, func: Callable[P, R]) -> Callable[P, R]: ...
     def log(self, level: LevelName | int, msg_template: str, attributes: dict[str, Any] | None = None, tags: Sequence[str] | None = None, exc_info: ExcInfo = False, console_log: bool | None = None) -> None:
         """Log a message.
 
