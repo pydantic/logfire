eef493337b1254bf87c1a622aea5814c58bb2a50
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2024-12-12 08:45:55 -0500

    Remove docs workaround and upgrade `mkdocs-material` (#662)
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- tests/otel_integrations/test_redis.py
+++ tests/otel_integrations/test_redis.py
@@ -34,7 +34,7 @@ def redis(redis_container: RedisContainer):
 
 @pytest.fixture
 def redis_port(redis_container: RedisContainer) -> str:
-    return redis_container.get_exposed_port(6379)
+    return redis_container.get_exposed_port(6379)  # pyright: ignore[reportReturnType]
 
 
 @pytest.fixture(autouse=True)
