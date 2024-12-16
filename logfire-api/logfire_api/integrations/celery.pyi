6b59d528d26766091f86ace0c7740bac021f165b
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-12-16 14:08:39 +0100

    remove kweags

--- logfire-api/logfire_api/integrations/celery.pyi
+++
@@ -1,5 +0,0 @@
-from typing import TypedDict
-
-class CeleryInstrumentKwargs(TypedDict, total=False):
-    """Keyword arguments for the [`logfire.instrument_celery`][logfire.Logfire.instrument_celery] function."""
-    skip_dep_check: bool
