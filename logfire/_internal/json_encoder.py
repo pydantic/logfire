6e506939fb8a56c802d374e7e8ff933a37c3d323
Alex Hall <alex.mojaki@gmail.com>
2024-12-27 13:04:05 +0200

    Handle errors from sqlalchemy.inspect

--- logfire/_internal/json_encoder.py
+++ logfire/_internal/json_encoder.py
@@ -161,11 +161,16 @@ def _pydantic_model_encoder(o: Any, seen: set[int]) -> JsonValue:
     return to_json_value(dump, seen)
 
 
-def _get_sqlalchemy_data(o: Any, seen: set[int]) -> JsonValue:
+def _get_sqlalchemy_data(o: Any, seen: set[int]) -> JsonValue | None:
+    if not is_sqlalchemy(o):
+        return None
     try:
-        from sqlalchemy import inspect as sa_inspect
+        from sqlalchemy import exc, inspect as sa_inspect
 
-        state = sa_inspect(o)
+        try:
+            state = sa_inspect(o)
+        except exc.NoInspectionAvailable:
+            return None
         deferred = state.unloaded
     except ModuleNotFoundError:  # pragma: no cover
         deferred = set()  # type: ignore
@@ -248,8 +253,10 @@ def to_json_value(o: Any, seen: set[int]) -> JsonValue:
                 key if isinstance(key, str) else safe_repr(key): to_json_value(value, seen)
                 for key, value in o.items()  # type: ignore
             }
-        elif is_sqlalchemy(o):
-            return _get_sqlalchemy_data(o, seen)
+
+        sa_data = _get_sqlalchemy_data(o, seen)
+        if sa_data is not None:
+            return sa_data
         elif dataclasses.is_dataclass(o):
             return {f.name: to_json_value(getattr(o, f.name), seen) for f in dataclasses.fields(o) if f.repr}
         elif is_attrs(o):
