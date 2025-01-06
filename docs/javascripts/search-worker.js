4e2e29e124f342a0a7501bb9853480d609d5dd08
Petyo Ivanov <underlog@gmail.com>
2025-01-06 14:23:28 +0200

    Client-side algolia integration

---
+++ docs/javascripts/search-worker.js
@@ -0,0 +1,56 @@
+importScripts('https://cdn.jsdelivr.net/npm/algoliasearch@5.18.0/dist/algoliasearch.umd.min.js')
+
+const SETUP = 0
+const READY = 1
+const QUERY = 2
+const RESULT = 3
+
+
+const appID = 'KPPUDTIAVX';
+// API key with `addObject` and `editSettings` ACL
+const apiKey = '1fc841595212a2c3afe8c24dd4cb8790';
+const indexName = 'logfire-docs';
+
+const client = algoliasearch.algoliasearch(appID, apiKey);
+
+self.onmessage = async (event) => {
+  if (event.data.type === SETUP) {
+    self.postMessage({ type: READY });
+  } else if (event.data.type === QUERY) {
+
+    const { results } = await client.search({
+      requests: [
+        {
+          indexName,
+          query: event.data.data,
+        },
+      ],
+    });
+
+    const hits = results[0].hits
+
+    const mappedGroupedResults = hits.reduce((acc, hit) => {
+      if (!acc[hit.pageID]) {
+        acc[hit.pageID] = []
+      }
+      acc[hit.pageID].push({
+        score: 1,
+        terms: {},
+        location: hit.abs_url,
+        title: hit.title,
+        text: hit.content,
+
+      })
+      return acc
+    }, {})
+
+
+    console.log(mappedGroupedResults)
+
+    self.postMessage({
+      type: RESULT, data: {
+        items: Object.values(mappedGroupedResults)
+      }
+    });
+  }
+};
