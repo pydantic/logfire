c03cef800470e31a7d08de4c1f9e0c4791bd0948
Mohamed Amine Zghal <medaminezghal@outlook.com>
2025-03-26 17:24:26 +0000

    Fix psycopg2 version test check for system package
    When you install psycopg2 from system packages or pip it will give you the same package as psycopg2-binary installed from pip. This will fix the test that use system site packages.

--- tests/otel_integrations/test_psycopg.py
+++ tests/otel_integrations/test_psycopg.py
@@ -13,7 +13,7 @@ from logfire._internal.integrations.psycopg import check_version
 
 
 def test_check_version():
-    assert check_version('psycopg2-binary', '2.7.3.1', Psycopg2Instrumentor())
+    assert check_version('psycopg2-binary', '2.7.3.1', Psycopg2Instrumentor())  or check_version('psycopg2', '2.7.3.1', Psycopg2Instrumentor()) 
     assert not check_version('psycopg2-binary', '2.7.3.0', Psycopg2Instrumentor())
     assert check_version('psycopg', '3.1.0', PsycopgInstrumentor())
     assert not check_version('psycopg', '3.0.1', PsycopgInstrumentor())
