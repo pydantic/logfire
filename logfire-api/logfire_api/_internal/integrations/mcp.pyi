b564fdbfde5b54e570ab803f8359a8d00e41b538
Alex Hall <alex.mojaki@gmail.com>
2025-04-10 11:36:36 +0200

    Bump version to v3.13.0

--- logfire-api/logfire_api/_internal/integrations/mcp.pyi
+++ logfire-api/logfire_api/_internal/integrations/mcp.pyi
@@ -1,3 +1,4 @@
-from logfire import Logfire as Logfire
+from logfire import LevelName as LevelName, Logfire as Logfire
+from logfire._internal.utils import handle_internal_errors as handle_internal_errors
 
 def instrument_mcp(logfire_instance: Logfire): ...
