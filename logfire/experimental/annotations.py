c9a2bc1c3ff668f60d5882e0e42ca9409a8b8db3
Alex Hall <alex.mojaki@gmail.com>
2025-04-11 13:12:27 +0200

    Feedback WIP (#994)

---
+++ logfire/experimental/annotations.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from typing import Any
+
+from opentelemetry.trace import Span, set_span_in_context
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+
+import logfire
+from logfire._internal.constants import ATTRIBUTES_MESSAGE_KEY, ATTRIBUTES_SPAN_TYPE_KEY, DISABLE_CONSOLE_KEY
+from logfire.propagate import attach_context
+
+TRACEPARENT_PROPAGATOR = TraceContextTextMapPropagator()
+TRACEPARENT_NAME = 'traceparent'
+assert TRACEPARENT_NAME in TRACEPARENT_PROPAGATOR.fields
+
+feedback_logfire = logfire.with_settings(custom_scope_suffix='feedback')
+
+
+def get_traceparent(span: Span | logfire.LogfireSpan) -> str:
+    """Get a string representing the span context to use for annotating spans."""
+    real_span: Span
+    if isinstance(span, Span):
+        real_span = span
+    else:
+        real_span = span._span  # type: ignore
+        assert real_span
+    context = set_span_in_context(real_span)
+    carrier: dict[str, Any] = {}
+    TRACEPARENT_PROPAGATOR.inject(carrier, context)
+    return carrier.get(TRACEPARENT_NAME, '')
+
+
+def raw_annotate_span(traceparent: str, span_name: str, message: str, attributes: dict[str, Any]) -> None:
+    """Create a span of kind 'annotation' as a child of the span with the given traceparent."""
+    with attach_context({TRACEPARENT_NAME: traceparent}, propagator=TRACEPARENT_PROPAGATOR):
+        feedback_logfire.info(
+            span_name,
+            **attributes,  # type: ignore
+            **{
+                ATTRIBUTES_MESSAGE_KEY: message,
+                ATTRIBUTES_SPAN_TYPE_KEY: 'annotation',
+                DISABLE_CONSOLE_KEY: True,
+            },
+        )
+
+
+def _record_feedback(  # type: ignore
+    traceparent: str,
+    name: str,
+    value: int | float | bool | str,
+    comment: str | None = None,
+    extra_attributes: dict[str, Any] | None = None,
+) -> None:  # pragma: no cover  # TODO
+    """VERY WIP, DO NOT USE.
+
+    Evaluate a span with a given value and reason.
+
+    Args:
+        traceparent: The traceparent string.
+        name: The name of the evaluation.
+        value: The value of the evaluation.
+            Numbers are interpreted as scores, strings as labels, and booleans as assertions.
+        comment: An optional reason for the evaluation.
+        extra_attributes: Optional additional attributes to include in the span.
+    """
+    attributes: dict[str, Any] = {'feedback.name': name}
+
+    if isinstance(value, bool):
+        attributes['feedback.assertion'] = value
+    elif isinstance(value, (int, float)):
+        attributes['feedback.score'] = value
+    else:
+        assert isinstance(value, str), f'Value must be a string, int, float, or bool, not {type(value)}'
+        attributes['feedback.label'] = value
+
+    if comment:
+        attributes['feedback.comment'] = comment
+
+    if extra_attributes:
+        attributes.update(extra_attributes)
+
+    raw_annotate_span(traceparent, 'feedback', f'feedback: {name}={value}', attributes)
---
+++ tests/test_annotations.py
@@ -0,0 +1,151 @@
+from inline_snapshot import snapshot
+from opentelemetry.trace import get_current_span
+
+import logfire
+from logfire._internal.exporters.test import TestExporter
+from logfire.experimental.annotations import (
+    _record_feedback,  # type: ignore
+    get_traceparent,
+    raw_annotate_span,
+)
+
+
+def test_get_traceparent(exporter: TestExporter):
+    with logfire.span('hi') as span:
+        traceparent = get_traceparent(span)
+        assert (
+            traceparent
+            == get_traceparent(get_current_span())
+            == snapshot('00-00000000000000000000000000000001-0000000000000001-01')
+        )
+
+    raw_annotate_span(traceparent, 'my_span_name', 'my_message', {'key': 'value'})
+
+    _record_feedback(
+        traceparent,
+        'factuality',
+        0.1,
+        comment='the mock agent lied',
+        extra_attributes={'agent_name': 'mock'},
+    )
+
+    # TODO there shouldn't be a pending span for the annotation
+    assert exporter.exported_spans_as_dict(_include_pending_spans=True) == snapshot(
+        [
+            {
+                'name': 'hi',
+                'context': {'trace_id': 1, 'span_id': 2, 'is_remote': False},
+                'parent': {'trace_id': 1, 'span_id': 1, 'is_remote': False},
+                'start_time': 1000000000,
+                'end_time': 1000000000,
+                'attributes': {
+                    'code.filepath': 'test_annotations.py',
+                    'code.function': 'test_get_traceparent',
+                    'code.lineno': 123,
+                    'logfire.msg_template': 'hi',
+                    'logfire.msg': 'hi',
+                    'logfire.span_type': 'pending_span',
+                    'logfire.pending_parent_id': '0000000000000000',
+                },
+            },
+            {
+                'name': 'hi',
+                'context': {'trace_id': 1, 'span_id': 1, 'is_remote': False},
+                'parent': None,
+                'start_time': 1000000000,
+                'end_time': 2000000000,
+                'attributes': {
+                    'code.filepath': 'test_annotations.py',
+                    'code.function': 'test_get_traceparent',
+                    'code.lineno': 123,
+                    'logfire.msg_template': 'hi',
+                    'logfire.msg': 'hi',
+                    'logfire.span_type': 'span',
+                },
+            },
+            {
+                'name': 'my_span_name',
+                'context': {'trace_id': 1, 'span_id': 4, 'is_remote': False},
+                'parent': {'trace_id': 1, 'span_id': 3, 'is_remote': False},
+                'start_time': 3000000000,
+                'end_time': 3000000000,
+                'attributes': {
+                    'logfire.span_type': 'pending_span',
+                    'logfire.level_num': 9,
+                    'logfire.msg_template': 'my_span_name',
+                    'logfire.msg': 'my_message',
+                    'code.filepath': 'test_annotations.py',
+                    'code.function': 'test_get_traceparent',
+                    'code.lineno': 123,
+                    'key': 'value',
+                    'logfire.disable_console_log': True,
+                    'logfire.json_schema': '{"type":"object","properties":{"key":{},"logfire.span_type":{},"logfire.disable_console_log":{}}}',
+                    'logfire.pending_parent_id': '0000000000000001',
+                },
+            },
+            {
+                'name': 'my_span_name',
+                'context': {'trace_id': 1, 'span_id': 3, 'is_remote': False},
+                'parent': {'trace_id': 1, 'span_id': 1, 'is_remote': True},
+                'start_time': 3000000000,
+                'end_time': 3000000000,
+                'attributes': {
+                    'logfire.span_type': 'annotation',
+                    'logfire.level_num': 9,
+                    'logfire.msg_template': 'my_span_name',
+                    'logfire.msg': 'my_message',
+                    'code.filepath': 'test_annotations.py',
+                    'code.function': 'test_get_traceparent',
+                    'code.lineno': 123,
+                    'key': 'value',
+                    'logfire.disable_console_log': True,
+                    'logfire.json_schema': '{"type":"object","properties":{"key":{},"logfire.span_type":{},"logfire.disable_console_log":{}}}',
+                },
+            },
+            {
+                'name': 'feedback',
+                'context': {'trace_id': 1, 'span_id': 6, 'is_remote': False},
+                'parent': {'trace_id': 1, 'span_id': 5, 'is_remote': False},
+                'start_time': 4000000000,
+                'end_time': 4000000000,
+                'attributes': {
+                    'logfire.span_type': 'pending_span',
+                    'logfire.level_num': 9,
+                    'logfire.msg_template': 'feedback',
+                    'logfire.msg': 'feedback: factuality=0.1',
+                    'code.filepath': 'test_annotations.py',
+                    'code.function': 'test_get_traceparent',
+                    'code.lineno': 123,
+                    'feedback.name': 'factuality',
+                    'feedback.score': 0.1,
+                    'feedback.comment': 'the mock agent lied',
+                    'agent_name': 'mock',
+                    'logfire.disable_console_log': True,
+                    'logfire.json_schema': '{"type":"object","properties":{"feedback.name":{},"feedback.score":{},"feedback.comment":{},"agent_name":{},"logfire.span_type":{},"logfire.disable_console_log":{}}}',
+                    'logfire.pending_parent_id': '0000000000000001',
+                },
+            },
+            {
+                'name': 'feedback',
+                'context': {'trace_id': 1, 'span_id': 5, 'is_remote': False},
+                'parent': {'trace_id': 1, 'span_id': 1, 'is_remote': True},
+                'start_time': 4000000000,
+                'end_time': 4000000000,
+                'attributes': {
+                    'logfire.span_type': 'annotation',
+                    'logfire.level_num': 9,
+                    'logfire.msg_template': 'feedback',
+                    'logfire.msg': 'feedback: factuality=0.1',
+                    'code.filepath': 'test_annotations.py',
+                    'code.function': 'test_get_traceparent',
+                    'code.lineno': 123,
+                    'feedback.name': 'factuality',
+                    'feedback.score': 0.1,
+                    'feedback.comment': 'the mock agent lied',
+                    'agent_name': 'mock',
+                    'logfire.disable_console_log': True,
+                    'logfire.json_schema': '{"type":"object","properties":{"feedback.name":{},"feedback.score":{},"feedback.comment":{},"agent_name":{},"logfire.span_type":{},"logfire.disable_console_log":{}}}',
+                },
+            },
+        ]
+    )
