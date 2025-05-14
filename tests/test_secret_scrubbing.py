87173b3939fd9b0722bf45cf81b0f3442fead0d4
Alex Hall <alex.mojaki@gmail.com>
2025-05-14 14:14:24 +0200

    Tighten scrubbing patterns to reduce accidental matches

--- tests/test_secret_scrubbing.py
+++ tests/test_secret_scrubbing.py
@@ -459,3 +459,55 @@ def test_fstring_magic_scrubbing(exporter: TestExporter):
             }
         ]
     )
+
+
+def test_word_boundaries(exporter: TestExporter):
+    logfire.info(
+        'hi',
+        x=[
+            'abcjwt',
+            'abc_jwt',
+            'abc-jwt',
+            'csrf123',
+            'csrf_123',
+            'csrf/123',
+            '456/csrf/123',
+        ],
+    )
+    assert exporter.exported_spans_as_dict(parse_json_attributes=True) == snapshot(
+        [
+            {
+                'name': 'hi',
+                'context': {'trace_id': 1, 'span_id': 1, 'is_remote': False},
+                'parent': None,
+                'start_time': 1000000000,
+                'end_time': 1000000000,
+                'attributes': {
+                    'logfire.span_type': 'log',
+                    'logfire.level_num': 9,
+                    'logfire.msg_template': 'hi',
+                    'logfire.msg': 'hi',
+                    'code.filepath': 'test_secret_scrubbing.py',
+                    'code.function': 'test_word_boundaries',
+                    'code.lineno': 123,
+                    'x': [
+                        'abcjwt',
+                        "[Scrubbed due to '_jwt']",
+                        "[Scrubbed due to 'jwt']",
+                        'csrf123',
+                        "[Scrubbed due to 'csrf_']",
+                        "[Scrubbed due to 'csrf']",
+                        "[Scrubbed due to 'csrf']",
+                    ],
+                    'logfire.json_schema': {'type': 'object', 'properties': {'x': {'type': 'array'}}},
+                    'logfire.scrubbed': [
+                        {'path': ['attributes', 'x', 1], 'matched_substring': '_jwt'},
+                        {'path': ['attributes', 'x', 2], 'matched_substring': 'jwt'},
+                        {'path': ['attributes', 'x', 4], 'matched_substring': 'csrf_'},
+                        {'path': ['attributes', 'x', 5], 'matched_substring': 'csrf'},
+                        {'path': ['attributes', 'x', 6], 'matched_substring': 'csrf'},
+                    ],
+                },
+            }
+        ]
+    )
