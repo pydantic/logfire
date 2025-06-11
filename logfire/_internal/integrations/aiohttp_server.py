72f5bd2a003652ae9b5c6f507530a31a5572aded
adtyavrdhn <adtyavrdhn@gmail.com>
2025-06-11 19:33:59 +0530

    Removing unused variable

--- logfire/_internal/integrations/aiohttp_server.py
+++ logfire/_internal/integrations/aiohttp_server.py
@@ -14,10 +14,9 @@ except ImportError:
         "You can install this with:\n"
         "    pip install 'logfire[aiohttp]'"
     )
-from logfire import Logfire
 
 
-def instrument_aiohttp_server(logfire_instance: Logfire, **kwargs: Any):
+def instrument_aiohttp_server(**kwargs: Any):
     """Instrument the `aiohttp` module so that spans are automatically created for each server request.
 
     See the `Logfire.instrument_aiohttp_server` method for details.
