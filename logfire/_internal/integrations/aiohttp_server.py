63bef955c88d3d8d38942b37ed61c2b02ca696e8
adtyavrdhn <adtyavrdhn@gmail.com>
2025-06-11 19:50:47 +0530

    Modified to catch_warnings + pragma_no_cover

--- logfire/_internal/integrations/aiohttp_server.py
+++ logfire/_internal/integrations/aiohttp_server.py
@@ -1,24 +1,22 @@
 from typing import Any
 import warnings
 
-warnings.filterwarnings(
-    "ignore",
-    message="Inheritance class _InstrumentedApplication from web.Application is discouraged",
-    category=DeprecationWarning,
-)
-try:
-    from opentelemetry.instrumentation.aiohttp_server import AioHttpServerInstrumentor
-except ImportError:
-    raise RuntimeError(
-        "`logfire.instrument_aiohttp_server()` requires the `opentelemetry-instrumentation-aiohttp-server` package.\n"
-        "You can install this with:\n"
-        "    pip install 'logfire[aiohttp]'"
-    )
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    try:
+        from opentelemetry.instrumentation.aiohttp_server import AioHttpServerInstrumentor
+    except ImportError:  # pragma: no cover
+        raise RuntimeError(
+            "`logfire.instrument_aiohttp_server()` requires the `opentelemetry-instrumentation-aiohttp-server` package.\n"
+            "You can install this with:\n"
+            "    pip install 'logfire[aiohttp]'"
+        )
+    from logfire import Logfire
 
+    def instrument_aiohttp_server(logfire_instance: Logfire, **kwargs: Any):
+        """Instrument the `aiohttp` module so that spans are automatically created for each server request.
 
-def instrument_aiohttp_server(**kwargs: Any):
-    """Instrument the `aiohttp` module so that spans are automatically created for each server request.
+        See the `Logfire.instrument_aiohttp_server` method for details.
+        """
 
-    See the `Logfire.instrument_aiohttp_server` method for details.
-    """
-    AioHttpServerInstrumentor().instrument(**kwargs)
+        AioHttpServerInstrumentor().instrument(**kwargs)
