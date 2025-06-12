4e8a27f28fa649cac234285ca7c3baf2fa5cbf97
Alex Hall <alex.mojaki@gmail.com>
2025-06-12 20:15:41 +0200

    Release v3.19.0 (#1140)

--- logfire-api/logfire_api/_internal/integrations/aiohttp_server.pyi
+++ logfire-api/logfire_api/_internal/integrations/aiohttp_server.pyi
@@ -1,7 +1,6 @@
-from logfire import Logfire as Logfire
 from typing import Any
 
-def instrument_aiohttp_server(logfire_instance: Logfire, **kwargs: Any):
+def instrument_aiohttp_server(**kwargs: Any):
     """Instrument the `aiohttp` module so that spans are automatically created for each server request.
 
     See the `Logfire.instrument_aiohttp_server` method for details.
