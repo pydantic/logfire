cc70e408eb801fe2d365229c4eeb6a7f210f1b7d
adtyavrdhn <adtyavrdhn@gmail.com>
2025-06-12 18:06:48 +0530

    Adding warning inside the try statement

--- logfire/_internal/integrations/aiohttp_server.py
+++ logfire/_internal/integrations/aiohttp_server.py
@@ -1,20 +1,21 @@
 import warnings
 from typing import Any
 
-with warnings.catch_warnings():
-    warnings.simplefilter('ignore')
-    try:
+try:
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
         from opentelemetry.instrumentation.aiohttp_server import AioHttpServerInstrumentor
-    except ImportError:  # pragma: no cover
-        raise RuntimeError(
-            '`logfire.instrument_aiohttp_server()` requires the `opentelemetry-instrumentation-aiohttp-server` package.\n'
-            'You can install this with:\n'
-            "    pip install 'logfire[aiohttp-server]'"
-        )
 
-    def instrument_aiohttp_server(**kwargs: Any):
-        """Instrument the `aiohttp` module so that spans are automatically created for each server request.
+except ImportError:  # pragma: no cover
+    raise RuntimeError(
+        '`logfire.instrument_aiohttp_server()` requires the `opentelemetry-instrumentation-aiohttp-server` package.\n'
+        'You can install this with:\n'
+        "    pip install 'logfire[aiohttp-server]'"
+    )
 
-        See the `Logfire.instrument_aiohttp_server` method for details.
-        """
-        AioHttpServerInstrumentor().instrument(**kwargs)
+def instrument_aiohttp_server(**kwargs: Any):
+    """Instrument the `aiohttp` module so that spans are automatically created for each server request.
+
+    See the `Logfire.instrument_aiohttp_server` method for details.
+    """
+    AioHttpServerInstrumentor().instrument(**kwargs)
