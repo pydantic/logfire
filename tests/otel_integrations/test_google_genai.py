b129fd733f6ed6856735b4c28753565f0129db0d
Alex Hall <alex.mojaki@gmail.com>
2025-07-02 15:44:20 +0200

    debugging

--- tests/otel_integrations/test_google_genai.py
+++ tests/otel_integrations/test_google_genai.py
@@ -2,10 +2,8 @@ import os
 
 import pydantic
 import pytest
-from dirty_equals import IsInt, IsPartialDict
 from inline_snapshot import snapshot
 
-import logfire
 from logfire._internal.utils import get_version
 from logfire.testing import TestExporter
 
@@ -24,8 +22,6 @@ def test_instrument_google_genai(exporter: TestExporter) -> None:
         pytest.skip('Requires newer opentelemetry semconv package')
 
     from google.genai import Client, types
-
-    logfire.instrument_google_genai()
 
     client = Client()
 
@@ -46,36 +42,3 @@ def test_instrument_google_genai(exporter: TestExporter) -> None:
     )
 
     assert response.text == snapshot('It is rainy in Boston, MA.\n')
-    assert exporter.exported_spans_as_dict(parse_json_attributes=True) == snapshot(
-        [
-            {
-                'name': 'generate_content gemini-2.0-flash-001',
-                'context': {'trace_id': 1, 'span_id': 1, 'is_remote': False},
-                'parent': None,
-                'start_time': IsInt(),
-                'end_time': 3000000000,
-                'attributes': {
-                    'code.function.name': 'google.genai.Models.generate_content',
-                    'gen_ai.system': 'gemini',
-                    'gen_ai.request.model': 'gemini-2.0-flash-001',
-                    'gen_ai.operation.name': 'chat',
-                    'logfire.span_type': 'span',
-                    'logfire.msg': 'generate_content gemini-2.0-flash-001',
-                    'gen_ai.usage.input_tokens': 58,
-                    'gen_ai.usage.output_tokens': 9,
-                    'gen_ai.response.finish_reasons': ('stop',),
-                    'logfire.metrics': IsPartialDict(),
-                    'events': [
-                        {'content': 'What is the weather like in Boston?', 'role': 'user'},
-                        {
-                            'index': 0,
-                            'finish_reason': 'STOP',
-                            'message': {'role': 'assistant', 'content': ['It is rainy in Boston, MA.\n']},
-                        },
-                    ],
-                    'logfire.json_schema': {'type': 'object', 'properties': {'events': {'type': 'array'}}},
-                    'gen_ai.response.model': 'gemini-2.0-flash-001',
-                },
-            }
-        ]
-    )
