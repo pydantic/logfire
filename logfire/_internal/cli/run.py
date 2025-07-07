47e6e8fa7d25d29c224cff6478e2219422f50a57
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-07-07 17:58:52 +0200

    revert suggestion

--- logfire/_internal/cli/run.py
+++ logfire/_internal/cli/run.py
@@ -174,9 +174,9 @@ def instrument_packages(installed_otel_packages: set[str], instrument_pkg_map: d
                 if hasattr(logfire, instrument_attr):
                     # The function exists, call it to instrument the package
                     getattr(logfire, instrument_attr)()
+                    instrumented.append(base_pkg)
             except Exception:  # pragma: no cover
                 continue
-            instrumented.append(base_pkg)
     return instrumented
 
 
