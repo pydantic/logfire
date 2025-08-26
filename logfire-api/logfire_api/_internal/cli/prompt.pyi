b75d502521b9973142cb77e325ce8041aa164cbb
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-08-26 09:50:52 +0200

    Bump version to v4.3.6

--- logfire-api/logfire_api/_internal/cli/prompt.pyi
+++ logfire-api/logfire_api/_internal/cli/prompt.pyi
@@ -1,5 +1,8 @@
 import argparse
 from logfire._internal.client import LogfireClient as LogfireClient
+from logfire.exceptions import LogfireConfigError as LogfireConfigError
+
+LOGFIRE_MCP_TOML: str
 
 def parse_prompt(args: argparse.Namespace) -> None:
     """Creates a prompt to be used with your favorite LLM.
