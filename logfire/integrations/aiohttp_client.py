340627b49d927d529e7ef7f56ed08e26d7ee361e
adtyavrdhn <adtyavrdhn@gmail.com>
2025-09-15 22:03:47 +0530

    fix

--- logfire/integrations/aiohttp_client.py
+++ logfire/integrations/aiohttp_client.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable
+from typing import Union
 
 from aiohttp.tracing import TraceRequestEndParams, TraceRequestExceptionParams, TraceRequestStartParams
 from multidict import CIMultiDict, CIMultiDictProxy
@@ -9,4 +10,4 @@ from opentelemetry.trace import Span
 AioHttpRequestHeaders = CIMultiDict[str]
 AioHttpResponseHeaders = CIMultiDictProxy[str]
 RequestHook = Callable[[Span, TraceRequestStartParams], None]
-ResponseHook = Callable[[Span, TraceRequestEndParams | TraceRequestExceptionParams], None]
+ResponseHook = Callable[[Span, Union[TraceRequestEndParams, TraceRequestExceptionParams]], None]
