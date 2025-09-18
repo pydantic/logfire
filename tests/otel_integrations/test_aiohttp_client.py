6b9f9997fb0cd9c0a83e194d36400d4c37816633
adtyavrdhn <adtyavrdhn@gmail.com>
2025-09-18 18:31:03 +0530

    Asserting response bodies

--- tests/otel_integrations/test_aiohttp_client.py
+++ tests/otel_integrations/test_aiohttp_client.py
@@ -306,8 +306,8 @@ async def test_aiohttp_client_capture_response_body(exporter: TestExporter, test
 
             async with aiohttp.ClientSession() as session:
                 async with session.get(f'http://localhost:{server.port}/body') as response:  # type: ignore
-                    await response.json()
-                    await response.read()
+                    assert await response.json() == {'good': 'response'}
+                    assert await response.json() == {'good': 'response'} # Triggering twice to ensure the body is captured only once
     finally:
         AioHttpClientInstrumentor().uninstrument()
 
