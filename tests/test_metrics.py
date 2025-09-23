4caae9a41722c57398b5008929255a408ba7f062
Viicos <65306057+Viicos@users.noreply.github.com>
2025-09-23 20:13:30 +0200

    Remove conditional check on OTEL version for `Gauge` import

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -17,7 +17,6 @@ from opentelemetry.sdk.metrics.export import (
 )
 
 import logfire
-import logfire._internal.metrics
 from logfire._internal.config import METRICS_PREFERRED_TEMPORALITY
 from logfire._internal.exporters.quiet_metrics import QuietMetricExporter
 from logfire._internal.exporters.test import TestExporter
@@ -203,17 +202,6 @@ def test_create_metric_gauge(metrics_reader: InMemoryMetricReader) -> None:
             }
         ]
     )
-
-
-def test_create_metric_gauge_old_opentelemetry_sdk(monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.setattr(logfire._internal.metrics, 'Gauge', None)
-    with pytest.raises(RuntimeError) as exc_info:
-        logfire.metric_gauge('gauge')
-    assert str(exc_info.value) == snapshot("""\
-Gauge is not available in this version of OpenTelemetry SDK.
-You should upgrade to 1.23.0 or newer:
-   pip install opentelemetry-sdk>=1.23.0\
-""")
 
 
 def test_create_metric_up_down_counter(metrics_reader: InMemoryMetricReader) -> None:
