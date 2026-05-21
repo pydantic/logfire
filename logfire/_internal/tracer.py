795be82cf9d286e15fbae24c9a614e5043b30cfc
Alex Hall <alex.mojaki@gmail.com>
2026-05-21 17:55:51 +0200

    patch traceback.format_exception instead

--- logfire/_internal/tracer.py
+++ logfire/_internal/tracer.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import functools
 import json
 import traceback
 from collections import defaultdict
@@ -480,44 +481,21 @@ def record_exception(
         else:
             span.set_attribute(ATTRIBUTES_EXCEPTION_FINGERPRINT_KEY, fingerprint)
 
+    span.record_exception(exception, attributes=attributes, timestamp=timestamp, escaped=escaped)
+
+
+original_format_exception = traceback.format_exception
+
+
+@functools.wraps(original_format_exception)
+def _patched_format_exception(*args: Any, **kwargs: Any):
     try:
-        span.record_exception(exception, attributes=attributes, timestamp=timestamp, escaped=escaped)
-    except Exception:
-        _otel_record_exception_patch(span, exception, attributes=attributes, timestamp=timestamp, escaped=escaped)
+        return original_format_exception(*args, **kwargs)
+    except Exception as exc:
+        return [f'Formatting stacktrace failed: {type(exc).__name__}: {exc}\n']
 
 
-def _otel_record_exception_patch(
-    span: trace_api.Span,
-    exception: BaseException,
-    attributes: otel_types.Attributes = None,
-    timestamp: int | None = None,
-    escaped: bool = False,
-) -> None:
-    """A copy of the OTel SDK's record_exception that handles errors in traceback.format_exception."""
-    from opentelemetry.semconv.attributes.exception_attributes import (
-        EXCEPTION_ESCAPED,
-        EXCEPTION_MESSAGE,
-        EXCEPTION_STACKTRACE,
-        EXCEPTION_TYPE,
-    )
-
-    try:
-        stacktrace = ''.join(traceback.format_exception(type(exception), value=exception, tb=exception.__traceback__))
-    except Exception as tb_exc:
-        stacktrace = f'Formatting stacktrace failed: {type(tb_exc).__name__}: {tb_exc}'
-
-    module = type(exception).__module__
-    qualname = type(exception).__qualname__
-    exception_type = f'{module}.{qualname}' if module and module != 'builtins' else qualname
-    _attributes: dict[str, otel_types.AttributeValue] = {
-        EXCEPTION_TYPE: exception_type,
-        EXCEPTION_MESSAGE: str(exception),
-        EXCEPTION_STACKTRACE: stacktrace,
-        EXCEPTION_ESCAPED: str(escaped),
-    }
-    if attributes:
-        _attributes.update(attributes)
-    span.add_event(name='exception', attributes=_attributes, timestamp=timestamp)
+traceback.format_exception = _patched_format_exception
 
 
 def set_exception_status(span: trace_api.Span, exception: BaseException):
