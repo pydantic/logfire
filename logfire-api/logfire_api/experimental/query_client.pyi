522d84378cf74868f3cd1f17cf68beb8ea7f9e5a
Alex Hall <alex.mojaki@gmail.com>
2026-08-28 14:55:40 +0200

    Keep generated query stubs unchanged

--- logfire-api/logfire_api/experimental/query_client.pyi
+++ logfire-api/logfire_api/experimental/query_client.pyi
@@ -4,9 +4,10 @@ from httpx import AsyncClient, Client, Response, Timeout
 from httpx._client import BaseClient
 from logfire import VERSION as VERSION
 from logfire._internal.config import get_base_url_from_token as get_base_url_from_token
+from logfire._internal.stack_info import warn_at_user_stacklevel as warn_at_user_stacklevel
 from pyarrow import Table
 from types import TracebackType
-from typing import Any, Generic, TypeVar, TypedDict
+from typing import Any, Generic, TypeVar, TypedDict, overload
 from typing_extensions import Self
 
 DEFAULT_TIMEOUT: Incomplete
@@ -60,68 +61,20 @@ class LogfireQueryClient(_BaseLogfireQueryClient[Client]):
     def __exit__(self, exc_type: type[BaseException] | None = None, exc_value: BaseException | None = None, traceback: TracebackType | None = None) -> None: ...
     def info(self) -> ReadTokenInfo:
         """Get information about the read token."""
-    def query_json_rows(self, sql: str, *, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, timezone: str | None = None, environment: str | list[str] | None = None) -> RowQueryResults:
-        """Query Logfire data and return the results as a row-oriented dictionary.
-
-        Args:
-            sql: The SQL `SELECT` query to execute.
-            min_timestamp: The minimum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            max_timestamp: The maximum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            limit: The maximum number of rows to query. This value takes priority over the
-                `LIMIT` clause in the `sql` query.
-            timezone: The timezone to use for the query execution context.
-            environment: Restrict rows to the provided environment(s). To only query rows where no environment is set,
-                use the empty string (`''`).
-
-        Returns:
-            A dictionary with two entries:
-              * `columns`: A list of column details including the name, datatype and whether the column is nullable.
-              * `rows`: The list of rows matching the query.
-        """
-    def query_arrow(self, sql: str, *, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, timezone: str | None = None, environment: str | list[str] | None = None) -> Table:
-        """Query Logfire data and return the results as a pyarrow Table.
-
-        Note that pyarrow must be installed for this method to succeed.
-
-        You can use `polars.from_arrow(result)` to convert the returned table to a polars DataFrame.
-
-        Args:
-            sql: The SQL `SELECT` query to execute.
-            min_timestamp: The minimum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            max_timestamp: The maximum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            limit: The maximum number of rows to query. This value takes priority over the
-                `LIMIT` clause in the `sql` query.
-            timezone: The timezone to use for the query execution context.
-            environment: Restrict rows to the provided environment(s). To only query rows where no environment is set,
-                use the empty string (`''`).
-        """
-    def query_csv(self, sql: str, *, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, timezone: str | None = None, environment: str | list[str] | None = None) -> str:
-        """Query Logfire data and return the results as a CSV-format string.
-
-        Use `polars.read_csv(StringIO(result))` to convert the returned CSV to a polars DataFrame.
-
-        Args:
-            sql: The SQL `SELECT` query to execute.
-            min_timestamp: The minimum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            max_timestamp: The maximum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            limit: The maximum number of rows to query. This value takes priority over the
-                `LIMIT` clause in the `sql` query.
-            timezone: The timezone to use for the query execution context.
-            environment: Restrict rows to the provided environment(s). To only query rows where no environment is set,
-                use the empty string (`''`).
-        """
+    def query_json(self, sql: str, min_timestamp: datetime | None = None, max_timestamp: datetime | None = None, limit: int | None = None) -> QueryResults:
+        """Query Logfire data and return the results as a column-oriented dictionary."""
+    @overload
+    def query_json_rows(self, sql: str, min_timestamp: None = None, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> RowQueryResults: ...
+    @overload
+    def query_json_rows(self, sql: str, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> RowQueryResults: ...
+    @overload
+    def query_arrow(self, sql: str, min_timestamp: None = None, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> Table: ...
+    @overload
+    def query_arrow(self, sql: str, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> Table: ...
+    @overload
+    def query_csv(self, sql: str, min_timestamp: None = None, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> str: ...
+    @overload
+    def query_csv(self, sql: str, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> str: ...
 
 class AsyncLogfireQueryClient(_BaseLogfireQueryClient[AsyncClient]):
     """An asynchronous client for querying Logfire data."""
@@ -130,7 +83,13 @@ class AsyncLogfireQueryClient(_BaseLogfireQueryClient[AsyncClient]):
     async def __aexit__(self, exc_type: type[BaseException] | None = None, exc_value: BaseException | None = None, traceback: TracebackType | None = None) -> None: ...
     async def info(self) -> ReadTokenInfo:
         """Get information about the read token."""
-    async def query_json_rows(self, sql: str, *, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, timezone: str | None = None, environment: str | list[str] | None = None) -> RowQueryResults:
+    @overload
+    async def query_json_rows(self, sql: str, min_timestamp: None = None, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> RowQueryResults: ...
+    @overload
+    async def query_json_rows(self, sql: str, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> RowQueryResults: ...
+    async def query_json(self, sql: str, min_timestamp: datetime | None = None, max_timestamp: datetime | None = None, limit: int | None = None) -> QueryResults:
+        """Query Logfire data and return the results as a column-oriented dictionary."""
+    async def query_json_rows(self, sql: str, min_timestamp: datetime | None = None, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> RowQueryResults:
         """Query Logfire data and return the results as a row-oriented dictionary.
 
         Args:
@@ -138,6 +97,10 @@ class AsyncLogfireQueryClient(_BaseLogfireQueryClient[AsyncClient]):
             min_timestamp: The minimum timestamp to use when querying data. If the provided
                 [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
                 be UTC.
+
+                /// version-deprecated | v4.35.0
+                Not providing a `min_timestamp` is deprecated.
+                ///
             max_timestamp: The maximum timestamp to use when querying data. If the provided
                 [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
                 be UTC.
@@ -152,43 +115,11 @@ class AsyncLogfireQueryClient(_BaseLogfireQueryClient[AsyncClient]):
               * `columns`: A list of column details including the name, datatype and whether the column is nullable.
               * `rows`: The list of rows matching the query.
         """
-    async def query_arrow(self, sql: str, *, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, timezone: str | None = None, environment: str | list[str] | None = None) -> Table:
-        """Query Logfire data and return the results as a pyarrow Table.
-
-        Note that pyarrow must be installed for this method to succeed.
-
-        You can use `polars.from_arrow(result)` to convert the returned table to a polars DataFrame.
-
-        Args:
-            sql: The SQL `SELECT` query to execute.
-            min_timestamp: The minimum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            max_timestamp: The maximum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            limit: The maximum number of rows to query. This value takes priority over the
-                `LIMIT` clause in the `sql` query.
-            timezone: The timezone to use for the query execution context.
-            environment: Restrict rows to the provided environment(s). To only query rows where no environment is set,
-                use the empty string (`''`).
-        """
-    async def query_csv(self, sql: str, *, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, timezone: str | None = None, environment: str | list[str] | None = None) -> str:
-        """Query Logfire data and return the results as a CSV-format string.
-
-        Use `polars.read_csv(StringIO(result))` to convert the returned CSV to a polars DataFrame.
-
-        Args:
-            sql: The SQL `SELECT` query to execute.
-            min_timestamp: The minimum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            max_timestamp: The maximum timestamp to use when querying data. If the provided
-                [`datetime`][datetime.datetime] doesn't have a timezone set, it is assumed to
-                be UTC.
-            limit: The maximum number of rows to query. This value takes priority over the
-                `LIMIT` clause in the `sql` query.
-            timezone: The timezone to use for the query execution context.
-            environment: Restrict rows to the provided environment(s). To only query rows where no environment is set,
-                use the empty string (`''`).
-        """
+    @overload
+    async def query_arrow(self, sql: str, min_timestamp: None = None, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> Table: ...
+    @overload
+    async def query_arrow(self, sql: str, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> Table: ...
+    @overload
+    async def query_csv(self, sql: str, min_timestamp: None = None, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> str: ...
+    @overload
+    async def query_csv(self, sql: str, min_timestamp: datetime, max_timestamp: datetime | None = None, limit: int | None = None, *, timezone: str | None = None, environment: str | list[str] | None = None) -> str: ...
